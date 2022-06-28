4fb180d697e8caa02b2dbbd7805280c816439a1d
Llyllyra <llyllyrallylly@gmail.com>
2022-06-28 16:27:53 +0200

    add Card avec image, nom et shortDescription

--- src/js/data.js
+++ src/js/data.js
@@ -1,27 +1,24 @@
-let body = document.querySelector("body")
+let body = document.querySelector(".grid-container")
 const setData = (data) => {
-    for (let datas of data){
-     let text = document.createElement("p");
-     let img = document.createElement("img")
-     text.innerText = `${datas.name}`;
-     text.innerHTML += `<br> ${datas.description}`;
-     text.innerHTML += `<br><br> ${datas.shortDescription}`;
-     img.setAttribute('src', `data:image/jpg;base64,${datas.image}`)
-     body.appendChild(text)
-     body.appendChild(img)
+    for (let datas of data) {
+        createCard(datas)
     }
 }
 
 
 const setDataById = (data) => {
-    let text = document.createElement("p");
+
+}
+
+const createCard = (data) => {
+    let section = document.createElement("section")
     let img = document.createElement("img")
-    text.innerText = `${data.name}`;
-    text.innerHTML += `<br> ${data.description}`;
-    text.innerHTML += `<br><br> ${data.shortDescription}`;
-    img.setAttribute('src', `data:image/jpg;base64,${data.image}`)
-    body.appendChild(text)
-    body.appendChild(img)
+    let cardBody = document.createElement("div")
+    section.classList.add("card")
+    body.appendChild(section)
+    section.innerHTML = `${data.name}
+    `
+    img.
 }
 
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import {getData, getDataById, getDataByName} from "./axios.js";
+import "../scss/main.scss";
 
 let recherche = "Lisa";
-//getData()
-getDataByName(recherche)
+getData()
+//getDataByName(recherche)
 //getDataById("f3e8d922-2b53-472e-9671-c0501a10451");
