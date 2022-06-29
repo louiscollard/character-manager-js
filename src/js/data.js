6b00bb13ada6659146d7637919e345dbc266621a
Louis Collard <louiscollard2@gmail.com>
2022-06-29 09:20:38 +0200

    Add afficher plus btn

--- src/js/data.js
+++ src/js/data.js
@@ -1,28 +1,24 @@
-let body = document.querySelector("body")
+let body = document.querySelector(".grid-container");
 const setData = (data) => {
-    for (let datas of data){
-     let text = document.createElement("p");
-     let img = document.createElement("img")
-     text.innerText = `${datas.name}`;
-     text.innerHTML += `<br> ${datas.description}`;
-     text.innerHTML += `<br><br> ${datas.shortDescription}`;
-     img.setAttribute('src', `data:image/jpg;base64,${datas.image}`)
-     body.appendChild(text)
-     body.appendChild(img)
-    }
-}
+	for (let datas of data) {
+		createCard(datas);
+	}
+};
 
+const setDataById = (data) => {};
 
-const setDataById = (data) => {
-    let text = document.createElement("p");
-    let img = document.createElement("img")
-    text.innerText = `${data.name}`;
-    text.innerHTML += `<br> ${data.description}`;
-    text.innerHTML += `<br><br> ${data.shortDescription}`;
-    img.setAttribute('src', `data:image/jpg;base64,${data.image}`)
-    body.appendChild(text)
-    body.appendChild(img)
-}
+const createCard = (data) => {
+	let section = document.createElement("section");
+	let img = document.createElement("img");
+	let cardBody = document.createElement("div");
+	let link = document.createElement("a");
+	link.classList.add("card-more");
+	section.classList.add("card");
+	body.appendChild(section);
+	link.innerHTML = `Afficher plus`;
+	section.innerHTML = `${data.name}
+    `;
+	section.appendChild(link);
+};
 
-
-export {setData, setDataById};
+export { setData, setDataById };
