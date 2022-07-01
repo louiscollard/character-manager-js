9c2c9bf4a62e1526e1634a124dfcf8c0bc9471da
Llyllyra <llyllyrallylly@gmail.com>
2022-07-01 10:17:25 +0200

    correct conflic checkout dev

--- src/js/axios.js
+++ src/js/axios.js
@@ -1,66 +1,58 @@
-import { setData, setDataByName } from "./data.js";
+import {setData, setDataByName} from "./data.js";
 import axios from "axios";
 
 let url = `https://character-database.becode.xyz/characters`;
-
+let grid = document.querySelector(".grid-container")
 let data = [];
 
 const getData = async () => {
-	try {
-		await axios.get(url)
-			.then((res) => {
-			data = res.data;
-				if (data.length !== 0) {
-					setData(data);
-				} else {
-					console.log(`Il n'y a aucun personnage dans L'API`);
-				}
-			})
-			.then(function () {
-				let btnMore = document.querySelectorAll(".card-more")
-				btnMore.forEach((e)=> {
-					e.addEventListener("click", () => {
-						let grid = document.querySelector(".grid-container")
-						grid.innerHTML = "";
-						let fullprofileName = e.parentNode.children[0].textContent;
-						axios.get(`${url}?name=${fullprofileName}`)
-							.then((res) => {
-								data = res.data;
-								console.log(data);
-								console.log(data.name);
-								if (data.length !== 0) {
-									setDataByName(data);
-								} else {
-									alert(`Le personnage rechercher n'existe pas`);
-									getData();
-								}
-							})
-					})
-				})
-			})
+    try {
+        await axios.get(url)
+            .then((res) => {
+                data = res.data;
+                if (data.length !== 0) {
+                    setData(data);
+                } else {
+                    grid.innerHTML =`Il n'y a aucun personnage dans L'API`;
+                }
+            })
 
-	} catch (error) {
-		console.error(error);
-	}
+            .then(function () {
+                let btnMore = document.querySelectorAll(".card-more")
+                btnMore.forEach((e) => {
+                    e.addEventListener("click", () => {
+                        let fullprofileName = e.parentNode.children[0].textContent;
+                        axios.get(`${url}?name=${fullprofileName}`)
+                            .then((res) => {
+                                data = res.data;
+                                if (data.length !== 0) {
+                                    const getID = res.data[0].id;
+                                    setDataByName(data);
+                                    let btnDel = document.querySelector(".del")
+                                    btnDel.addEventListener("click", () => {
+                                        if (confirm(`Etes vous sûr de vouloir supprimer ${res.data[0].name}?`)) {
+                                            axios.delete(
+                                                `https://character-database.becode.xyz/characters/${getID}`
+                                            );
+                                            alert(`${res.data[0].name} à bien été supprimer`);
+                                        }
+                                        getData();
+                                    })
+                                } else {
+                                    alert(`Le personnage rechercher n'existe pas`);
+                                    getData();
+                                }
+                            })
+                    })
+
+                })
+            })
+
+    } catch (error) {
+        console.error(error);
+    }
 
 };
 
 
-
-const getDataById = async (id) => {
-	try {
-		await axios.get(`${url}/${id}`).then((res) => {
-			data = res.data;
-		});
-	} catch (error) {
-		console.error("id pas trouvé");
-	}
-	if (data.length !== 0) {
-		setDataById(data);
-	} else {
-		alert(`L'id rechercher n'existe pas`);
-		await getData();
-	}
-};
-
-export { getData, getDataById};
+export {getData};
--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,5 @@ import "../scss/main.scss";
 import { getData} from "./axios.js";
 import { toggleModal, modalContainer, modalTriggers } from "./add.js";
 import "./search.js";
-
-let recherche = "Lisa";
 getData();
 
